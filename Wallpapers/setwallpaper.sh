8770213ee0a6618f3c89e6cb59e75051ffaac625
stefan911 <danielstefan.halasz@gmail.com>
2022-01-18 20:25:59 +0200

    Removed feh functionality to create bg script

--- Wallpapers/setwallpaper.sh
+++ Wallpapers/setwallpaper.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 WALLPAPERS_PATH="/home/stefan911/Wallpapers"
 CURR_WALLPAPER=$(cat $WALLPAPERS_PATH/wallpaper)
-feh --bg-fill $WALLPAPERS_PATH/$CURR_WALLPAPER
+feh --no-fehbg --bg-fill $WALLPAPERS_PATH/$CURR_WALLPAPER
