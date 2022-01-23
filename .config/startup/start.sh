63469dd82872def50b46b1807ba7ba05155eb47a
stefan911 <danielstefan.halasz@gmail.com>
2022-01-23 23:26:58 +0200

    Added mouse sensitivity setting

--- .config/startup/start.sh
+++ .config/startup/start.sh
@@ -2,5 +2,8 @@
 # Set screen wallpaper
 /home/stefan911/Wallpapers/./setwallpaper.sh
 
+# Set mouse sens
+xinput --set-prop 11 'libinput Accel Speed' -0.8
+
 # Start KeepassXC in systemtray
 keepassxc &
