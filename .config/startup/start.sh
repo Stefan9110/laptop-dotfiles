b29298907012509e7b8095191fe4efd6a9f11382
stefan911 <danielstefan.halasz@gmail.com>
2022-02-03 20:36:23 +0200

    Modified rice to fit laptop needs

--- .config/startup/start.sh
+++ .config/startup/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Set screen wallpaper
-/home/stefan911/Wallpapers/./setwallpaper.sh
+$HOME/Wallpapers/./setwallpaper.sh
 
 # Set mouse sens
 xinput --set-prop 11 'libinput Accel Speed' -0.8
--- Wallpapers/setwallpaper.sh
+++ Wallpapers/setwallpaper.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-WALLPAPERS_PATH="/home/stefan911/Wallpapers"
+WALLPAPERS_PATH="$HOME/Wallpapers"
 CURR_WALLPAPER=$(cat $WALLPAPERS_PATH/wallpaper)
 feh --no-fehbg --bg-fill $WALLPAPERS_PATH/$CURR_WALLPAPER
